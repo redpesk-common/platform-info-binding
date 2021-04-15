fb805b757aff5b278c3b68b48ffa38e7cf0ec335
vlefebvre <valentin.lefebvre@iot.bzh>
2021-04-15 10:55:48 +0200

    adding list of plugins installed
    Signed-off-by: vlefebvre <valentin.lefebvre@iot.bzh>

--- scripts/afm_packages_installed.sh
+++ scripts/afm_packages_installed.sh
@@ -6,21 +6,37 @@ function list_afm_packaged() {
     for packages in $(ls /var/local/lib/afm/applications/); do
         fields=()
         if [[ "$packages" != "include" ]] && [[  "$packages" != "recovery" ]]; then
-                FullNamePackage=$(rpm -qf /var/local/lib/afm/applications/$packages)
-		package=""
-            	for tmp in $(echo $FullNamePackage | awk -F'-' '{for(i=1; i<=NF-2; ++i) print $i}'); do
-                	if [[ $package == "" ]]
-                	then
-                    		package="$tmp"
-                	else
-                        	package="$package-$tmp"
-                	fi
-            	done
-                Version=$(rpm --info -q ${packages} | grep Version | awk -F' ' '{print $3}')
-                echo "{\"${package}\": \"${Version}\"}"
+            FullNamePackage=$(rpm -qf /var/local/lib/afm/applications/$packages)
+            package=""
+            for tmp in $(echo $FullNamePackage | awk -F'-' '{for(i=1; i<=NF-2; ++i) print $i}'); do
+                if [[ $package == "" ]]
+                then
+                        package="$tmp"
+                else
+                        package="$package-$tmp"
+                fi
+            done
+            Version=$(rpm --info -q ${packages} | grep Version | awk -F' ' '{print $3}')
+            echo "{\"${package}\": \"${Version}\"}"
+
+            # Check if plugins
+            if [[ -d /var/local/lib/afm/applications/$packages/lib/plugins ]]; then
+                for plugins in $(ls /var/local/lib/afm/applications/$packages/lib/plugins); do
+                    FullNamePlugin=$(rpm -qf /var/local/lib/afm/applications/$packages/lib/plugins/$plugins)
+                    plugin=""
+                    for tmp in $(echo $FullNamePlugin | awk -F'-' '{for(i=1; i<=NF-2; ++i) print $i}'); do
+                            if [[ $plugin == "" ]]; then
+                                    plugin="$tmp"
+                            else
+                                    plugin="$plugin-$tmp"
+                            fi
+                    done
+                    Version=$(rpm --info -q ${FullNamePlugin} | grep Version | awk -F' ' '{print $3}')
+                    [[ $plugin != $package ]] && echo "{\"${plugin}\": \"${Version}\"}"
+                done
+            fi
         fi
     done
 }
 
 list_afm_packaged
-
